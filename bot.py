d5a655805e6e3009eba3ea6498242a5b1f7375fe
Alex <103825@glr.nl>
2026-04-21 19:11:16 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+import os
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(1495881024201031793)
+    if channel:
+        await channel.send(f"Fakka {member.mention}!")
+
+bot.run(os.getenv("DISCORd_TOKEN"))
